0c642dd4dc5ac1206bdc65ee4e504917a594470f
PotassiumIodide <imamura.suuri@gmail.com>
2020-09-18 20:38:34 +0900

    Partially implemented the codes in Chapter4 by Python3. But something wrong with program4_4.py and program4_5.

---
+++ src/ch04/program4_4.py
@@ -0,0 +1,40 @@
+from math import cos
+
+EPS  = 10.0 ** -8.0 # epsilon の設定
+NMAX = 100          # 最大反復回数
+
+def main():
+    print("初期値 x0 と x1 を入力してください")
+    x0, x1 = map(float, input().split())
+
+    secant( x0, x1 )
+
+
+# 割線法
+def secant(x0: float, x1: float):
+    global EPS, NMAX
+    n = 1
+    d = x1 - x0
+    y0 = f(x0)
+
+    while True:
+        y1 = f(x1)
+        d = -d * y1 / (y1 - y0)
+        x1 = x1 + d
+        y0 = y1
+        n += 1
+        if abs(d) <= EPS or n >= NMAX:
+            break
+
+    if n == NMAX:
+        print("答えが見つかりませんでした")
+    else:
+        print(f"答えは x={x1} です")
+
+
+def f(x: float) -> float:
+    return x - cos(x)
+
+
+if __name__ == "__main__":
+    main()
